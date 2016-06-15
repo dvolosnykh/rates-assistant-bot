5e57aa57c413c581fef8ec3e66200b3b23c75c47
Dmitry Volosnykh <dmitry.volosnykh@gmail.com>
2016-06-15 22:35:32 +0300

    Add /cbr path.

--- ratesassistant.py
+++ ratesassistant.py
@@ -1,14 +1,44 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, abort
+from flask_restful import Resource, Api, reqparse
+from lxml import etree
+import requests
+
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app, '/ratesassistant')
 
+
+@api.resource('/cbr', '/cbr/<currency>')
 class CentralBankRussia(Resource):
-    def get(self):
-        return {'rates': 'no rates yet'}
+    def get(self, currency=None):
+        if currency is None:
+            currency = 'usd'
+        currency = currency.upper()
 
-api.add_resource(CentralBankRussia, '/cbr')
+        parser = reqparse.RequestParser()
+        parser.add_argument('date')
+        args = parser.parse_args()
+
+        request_date = args.get('date')
+        if request_date is None:
+            response = requests.get('http://www.cbr.ru/scripts/XML_daily_eng.asp')
+        else:
+            response = requests.get('http://www.cbr.ru/scripts/XML_daily_eng.asp?date_req={}'.format(request_date))
+        doc = etree.fromstring(response.content)
+
+        currency_path = "//Valute[CharCode = '{}']/Value/text()".format(currency)
+        currencies = doc.xpath(currency_path)
+        if len(currencies) > 0:
+            date = doc.xpath('/ValCurs/@Date')[0]
+            rate = float(currencies[0].replace(',', '.'))
+            return {
+                'currency': currency,
+                'date': date,
+                'rate': rate
+            }
+        else:
+            abort(404)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
